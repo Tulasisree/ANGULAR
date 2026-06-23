af692577b1c113dce7eb8ad698f9ff4223dbd864
Tulasisree <sree.mulagapaka@gmail.com>
2026-06-23 23:29:22 +0530

    Hotel Reservation App

---
+++ hotel-app/src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from "../app-routing.module";
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  exports:[
+    HomeComponent
+  ]
+})
+export class HomeModule { }
---
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+  private reservations: Reservation[] = [];
+  
+  constructor(){
+    let savedReservations = localStorage.getItem("reservations")
+    this.reservations = savedReservations? JSON.parse(savedReservations):[]
+  }
+
+  //CRUD
+
+  getReservations(): Reservation[]{
+    return this.reservations;
+  }
+
+  // chances are we can return a resvertation , undefined if we don't find it in array
+  getReservation(id: string):Reservation | undefined{
+    return this.reservations.find(res=> res.id == id)
+  }
+
+  addReservation(reservation: Reservation){
+    reservation.id = Date.now().toString();
+    this.reservations.push(reservation)
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+  }
+
+  deleteReservation(id:string){
+    let index= this.reservations.findIndex(res=> res.id === id)
+    this.reservations.splice(index,1)
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+  }
+
+  updateReservation(id:string,updatedReservation:Reservation){
+    let index = this.reservations.findIndex(res=> res.id === id)
+    this.reservations[index] = updatedReservation
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+  }
+}
