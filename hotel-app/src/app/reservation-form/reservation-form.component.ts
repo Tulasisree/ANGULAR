d0982c9cc1a648c23cc5cfd426b4661328c27cdd
Tulasisree <sree.mulagapaka@gmail.com>
2026-06-24 12:23:21 +0530

    Adding Observables to Reservation app

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -30,9 +30,10 @@ export class ReservationFormComponent implements OnInit{
 
     let id = this.activatedRoute.snapshot.paramMap.get('id')
     if(id){
-      let reservation = this.reservationService.getReservation(id)
-      if(reservation)
-        this.reservationForm.patchValue(reservation)
+      this.reservationService.getReservation(id)?.subscribe(reservation => {
+        if(reservation)
+          this.reservationForm.patchValue(reservation)
+      })
     }
   }
 
@@ -42,9 +43,13 @@ export class ReservationFormComponent implements OnInit{
       let reservation:Reservation = this.reservationForm.value;
       let id = this.activatedRoute.snapshot.paramMap.get('id')
       if(id){
-        this.reservationService.updateReservation(id,reservation);
+        this.reservationService.updateReservation(id,reservation).subscribe(()=>{
+          console.log("Update request processed")
+        });
       }else{
-        this.reservationService.addReservation(reservation);
+        this.reservationService.addReservation(reservation).subscribe(()=>{
+          console.log("Adding reservation processed")
+        });
       }
 
       this.router.navigate(['list'])
--- hotel-app/src/app/reservation/reservation.service.ts
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -1,43 +1,60 @@
 import { Injectable } from '@angular/core';
 import { Reservation } from '../models/reservation';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
+  //API URL (pointing tou our mockoon api)
+  private apiUrl = "http://localhost:3000"
+
   private reservations: Reservation[] = [];
   
-  constructor(){
-    let savedReservations = localStorage.getItem("reservations")
-    this.reservations = savedReservations? JSON.parse(savedReservations):[]
-  }
+  // constructor(){
+  //   let savedReservations = localStorage.getItem("reservations")
+  //   this.reservations = savedReservations? JSON.parse(savedReservations):[]
+  // }
+
+  constructor(private http: HttpClient){}
 
   //CRUD
-
-  getReservations(): Reservation[]{
-    return this.reservations;
+  getReservations(): Observable<Reservation[]>{
+    // return this.reservations;
+    return this.http.get<Reservation[]>(this.apiUrl + "/reservations")
   }
 
   // chances are we can return a resvertation , undefined if we don't find it in array
-  getReservation(id: string):Reservation | undefined{
-    return this.reservations.find(res=> res.id == id)
+  getReservation(id: string):Observable<Reservation> | undefined{
+    //return this.reservations.find(res=> res.id == id)
+    return this.http.get<Reservation>(this.apiUrl + "/reservation/"+id)
   }
 
   addReservation(reservation: Reservation){
+
+    return this.http.post<void>(this.apiUrl + "/reservation/",reservation)
+
     reservation.id = Date.now().toString();
     this.reservations.push(reservation)
-    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+    // localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 
-  deleteReservation(id:string){
+  deleteReservation(id:string): Observable<void>{
+
+    return this.http.delete<void>(this.apiUrl+"/reservation/"+id)
+
     let index= this.reservations.findIndex(res=> res.id === id)
     this.reservations.splice(index,1)
-    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+    // localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 
   updateReservation(id:string,updatedReservation:Reservation){
+
+    return this.http.put<void>(this.apiUrl + "/reservation/"+id,updatedReservation)
+
     let index = this.reservations.findIndex(res=> res.id === id)
     this.reservations[index] = updatedReservation
-    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+    // localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 }
